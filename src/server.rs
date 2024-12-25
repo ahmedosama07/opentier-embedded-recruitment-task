025aa3f7f98391b49654d22cdc65dc9363056202
Ahmed Osama <32176872+ahmedosama07@users.noreply.github.com>
2024-12-25 19:24:40 +0200

    refactor server.rs

--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,5 @@
 use crate::message::{
-    AddResponse, ClientMessage, EchoMessage, ServerMessage
+    AddResponse, ClientMessage, ServerMessage
 };
 
 use crate::message::client_message::Message as ClientMessageEnum;
@@ -22,6 +22,8 @@ use std::{
 
 use lazy_static::lazy_static;
 
+const MAX_MESSAGE_SIZE: usize = 1024 * 1024;
+
 lazy_static! {
     static ref SERVERS: Arc<Mutex<HashMap<String, Arc<Server>>>> = Arc::new(Mutex::new(HashMap::new()));
 }
@@ -31,58 +33,88 @@ struct Client {
 }
 
 impl Client {
+    fn read_message(&mut self) -> io::Result<Vec<u8>> {
+        let mut len_buf = [0u8; 4];
+        self.stream.read_exact(&mut len_buf)?;
+
+        let message_len = u32::from_be_bytes(len_buf) as usize;
+        if message_len > MAX_MESSAGE_SIZE {
+            return Err(io::Error::new(
+                ErrorKind::InvalidData,
+                "Message size exceeds maximum allowed",
+            ));
+        }
+
+        let mut buffer = vec![0; message_len];
+        self.stream.read_exact(&mut buffer)?;
+        Ok(buffer)
+    }
+
+    fn write_message(&mut self, payload: &[u8]) -> io::Result<()> {
+        let len = payload.len() as u32;
+        self.stream.write_all(&len.to_be_bytes())?;
+        self.stream.write_all(payload)?;
+        self.stream.flush()
+    }
+
     pub fn new(stream: TcpStream) -> Self {
         Client { stream }
     }
 
-    pub fn handle(&mut self) -> io::Result<()> {
-        let mut buffer = [0; 512];
-        // Read data from the client
-        let bytes_read = self.stream.read(&mut buffer)?;
-        if bytes_read == 0 {
-            info!("Client disconnected.");
-            return Ok(());
-        }
-
-        match ClientMessage::decode(&buffer[..bytes_read]) {
-            Ok(client_message) => {
-                info!("Decoded message from client: {:?}", client_message);
-                if let Some(client_msg) = client_message.message {
-                    let response_message = self.process_message(client_msg);
-                    self.send_response(ServerMessage { message: Some(response_message) })?;
+    pub fn handle(&mut self) -> io::Result<bool> {
+        self.stream.set_nonblocking(false)?;
+        match self.read_message() {
+            Ok(buffer) => match ClientMessage::decode(&buffer[..]) {
+                Ok(client_msg) => {
+                    if let Some(response) = self.process_message(client_msg.message) {
+                        let encoded = response.encode_to_vec();
+                        self.write_message(&encoded)?;
+                    } else {
+                        warn!("Received empty message");
+                    }
+                    Ok(true)
+                }
+                Err(e) => {
+                    error!("Failed to decode message: {}", e);
+                    Ok(false)
+                }
+            },
+            Err(e) => {
+                if e.kind() == ErrorKind::UnexpectedEof {
+                    Ok(false)
                 } else {
-                    warn!("Received an empty message.");
+                    Err(e)
                 }
             }
-            Err(err) => {
-                error!("Failed to decode ClientMessage. Error: {:?}, Data: {:?}", err, &buffer[..bytes_read]);
-            }
         }
-
-        Ok(())
     }
 
-    fn process_message(&self, client_msg: ClientMessageEnum) -> ServerMessageEnum {
-        match client_msg {
-            ClientMessageEnum::EchoMessage(echo) => {
-                info!("Processing EchoMessage.");
-                ServerMessageEnum::EchoMessage(EchoMessage { content: echo.content })
+    fn process_message(
+        &mut self,
+        message: Option<ClientMessageEnum>,
+    ) -> Option<ServerMessage> {
+        match message {
+            Some(ClientMessageEnum::EchoMessage(echo)) => {
+                info!("Handling echo message: {}", echo.content);
+                Some(ServerMessage {
+                    message: Some(ServerMessageEnum::EchoMessage(echo)),
+                })
             }
-            ClientMessageEnum::AddRequest(add) => {
+            Some(ClientMessageEnum::AddRequest(add)) => {
+                info!("Handling add request: {} + {}", add.a, add.b);
                 let result = add.a + add.b;
-                info!("Processing AddRequest. Result: {}", result);
-                ServerMessageEnum::AddResponse(AddResponse { result })
+                Some(ServerMessage {
+                    message: Some(ServerMessageEnum::AddResponse(AddResponse { result })),
+                })
+            }
+            None => {
+                warn!("No message found in ClientMessage.");
+                None
             }
         }
-    }
-
-    fn send_response(&mut self, response: ServerMessage) -> io::Result<()> {
-        let mut buf = Vec::new();
-        response.encode(&mut buf)?;
-        self.stream.write_all(&buf)?;
-        Ok(())
     }
 }
+
 
 #[derive(Debug)]
 pub struct Server {
@@ -94,103 +126,123 @@ pub struct Server {
 impl Server {
     /// Creates a new server instance
     pub fn new(addr: &str) -> io::Result<Arc<Self>> {
-        let mut servers_lock = SERVERS.lock().unwrap();
+        let servers_lock = SERVERS.lock().unwrap();
         info!("Server instances: {:?}", *servers_lock);
 
         if let Some(server) = servers_lock.get(addr) {
             warn!("Server address {} already in use.", addr);
-            {
-                let mut  cnt = server.client_cnt.lock().unwrap();
-                *cnt += 1;
-            }
-            return  Ok(Arc::clone(server));
+            Self::increment_client_count(server);
+            return Ok(Arc::clone(server));
         }
+
+        Self::create_and_register_server(addr, servers_lock)
+    }
+
+    fn increment_client_count(server: &Arc<Server>) {
+        let mut cnt = server.client_cnt.lock().unwrap();
+        *cnt += 1;
+    }
+
+    fn create_and_register_server(
+        addr: &str,
+        mut servers_lock: MutexGuard<'_, HashMap<String, Arc<Server>>>,
+    ) -> io::Result<Arc<Self>> {
         match TcpListener::bind(addr) {
             Ok(listener) => {
-                let is_running = Arc::new(AtomicBool::new(false));
-                let client_cnt = Arc::new(Mutex::new(1));
                 let server = Arc::new(Server {
                     listener,
-                    is_running,
-                    client_cnt,
+                    is_running: Arc::new(AtomicBool::new(false)),
+                    client_cnt: Arc::new(Mutex::new(1)),
                 });
                 servers_lock.insert(addr.to_string(), Arc::clone(&server));
                 Ok(server)
             }
-            Err(ref e) if e.kind() == ErrorKind::AddrInUse => {
-                eprintln!("Address {} is already in use.", addr);
-                Err(io::Error::new(e.kind(), e.to_string()))
-            }
             Err(e) => {
-                eprintln!("Failed to bind to address {}: {}", addr, e);
-                Err(e)
-            }    
+                let msg = format!(
+                    "Failed to bind to address {}: {}",
+                    addr,
+                    if e.kind() == ErrorKind::AddrInUse {
+                        "Address is already in use"
+                    } else {
+                        "Unknown error"
+                    }
+                );
+                error!("{}", msg);
+                Err(io::Error::new(e.kind(), msg))
+            }
         }
     }
 
-
     /// Runs the server, listening for incoming connections and handling them
     pub fn run(&self) -> io::Result<()> {
-        self.is_running.store(true, Ordering::SeqCst); // Set the server as running
-        info!("Server is running on {}", self.listener.local_addr()?);
-
-        // Set the listener to non-blocking mode
+        self.start();
         self.listener.set_nonblocking(true)?;
 
         while self.is_running.load(Ordering::SeqCst) {
-            match self.listener.accept() {
-                Ok((stream, addr)) => {
-                    info!("New client connected: {}", addr);
-
-                    // Handle the client request
-                    let is_running = Arc::clone(&self.is_running);
-        
-                    // Spawn a new thread to handle the client connection
-                    thread::spawn(move || {
-                        let mut client = Client::new(stream);
-                        while is_running.load(Ordering::SeqCst) {
-                            if let Err(e) = client.handle() {
-                                error!("Error handling client: {}", e);
-                                break;
-                            }
-                        }
-                    });
-                }
-                Err(ref e) if e.kind() == ErrorKind::WouldBlock => {
-                    // No incoming connections, sleep briefly to reduce CPU usage
-                    thread::sleep(Duration::from_millis(100));
-                }
-                Err(e) => {
-                    error!("Error accepting connection: {}", e);
-                }
-            }
+            self.accept_client()?;
         }
 
         info!("Server stopped.");
         Ok(())
     }
 
+    fn start(&self) {
+        self.is_running.store(true, Ordering::SeqCst);
+        info!("Server is running on {}", self.listener.local_addr().unwrap());
+    }
+
+    fn accept_client(&self) -> io::Result<()> {
+        match self.listener.accept() {
+            Ok((stream, addr)) => {
+                info!("New client connected: {}", addr);
+                self.handle_client_connection(stream);
+            }
+            Err(ref e) if e.kind() == ErrorKind::WouldBlock => {
+                thread::sleep(Duration::from_millis(100));
+            }
+            Err(e) => {
+                error!("Error accepting connection: {}", e);
+                return Err(e);
+            }
+        }
+        Ok(())
+    }
+
+    fn handle_client_connection(&self, stream: TcpStream) {
+        let is_running = Arc::clone(&self.is_running);
+
+        thread::spawn(move || {
+            let mut client = Client::new(stream);
+            while is_running.load(Ordering::SeqCst) {
+                if let Err(e) = client.handle() {
+                    error!("Error handling client: {}", e);
+                    break;
+                }
+            }
+        });
+    }
+
     /// Stops the server by setting the `is_running` flag to `false`
     pub fn stop(&self) {
         let mut cnt = self.client_cnt.lock().unwrap();
         if *cnt == 1 {
-            if self.is_running.load(Ordering::SeqCst) {
-                self.is_running.store(false, Ordering::SeqCst);
-                info!("Shutdown signal sent.");
-
-                let mut server_lock:MutexGuard<'_, HashMap<String, Arc<Server>>> = SERVERS.lock().unwrap();
-                let addr = self.listener.local_addr().unwrap().to_string();
-                server_lock.remove(&addr);
-            } else {
-                warn!("Server was already stopped or not running.");
-            }
+            self.shutdown();
         } else {
-            {
-                *cnt -= 1;
-                info!("Client disconnected.");
-            }
-            info!("Clients: {}", *cnt)
+            *cnt -= 1;
+            info!("Client disconnected. Remaining clients: {}", *cnt);
         }
-        
+    }
+
+    fn shutdown(&self) {
+        if self.is_running.load(Ordering::SeqCst) {
+            self.is_running.store(false, Ordering::SeqCst);
+            info!("Shutdown signal sent.");
+
+            let mut server_lock = SERVERS.lock().unwrap();
+            let addr = self.listener.local_addr().unwrap().to_string();
+            server_lock.remove(&addr);
+        } else {
+            warn!("Server was already stopped or not running.");
+        }
     }
 }
